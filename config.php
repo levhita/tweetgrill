0ef00a4ed2ece676f102a6013d32d3b3b294bb06
Levhita <levhita@gmail.com>
2015-07-29 03:39:24 -0500

    deleted config

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-/** Config file for tweetgrill, copy as config.php and add your parameters **/
-$db_host 	 = "localhost";
-$db_name 	 = "tweetgrill";
-$db_username = "vagrant";
-$db_password = "vagrant123";
